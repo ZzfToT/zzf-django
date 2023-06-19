e69ecd283d9e697f879d933539035a82a236266d
zzf <zhoufeizzf@163.com>
2023-06-19 16:35:24 +0800

    homepage

--- urls.py
+++ urls.py
@@ -1,5 +1,5 @@
-from django.urls import path
-from myapp.views import post, latest_post, tagsandcates, postlist_all, category, tag
+from django.urls import path, re_path
+from myapp.views import post, latest_post, tagsandcates, postlist_all, category, tag, homepage
 
 urlpatterns = [
         path('post/', post.index, name="post_index"),
@@ -7,6 +7,7 @@ urlpatterns = [
         path('tagsandcates/', tagsandcates.index, name="tagsandcates_index"),
         path('postlistall/', postlist_all.index, name="postlist_all_idnex"),
         path('category/', category.index, name="category_index"),
-        path('tag/', tag.index, name="tag_index")
+        path('tag/', tag.index, name="tag_index"),
+        re_path('.*', homepage.index, name="homepage_index")
 ]
 
